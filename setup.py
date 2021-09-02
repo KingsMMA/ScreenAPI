52b65d725284b35c846b794530ebe6b9ca8c96db
KingsMMA <63374258+KingsMMA@users.noreply.github.com>
2021-09-03 09:22:06 +1000

    Initial commit
    First commit.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name = "ScreenAPI",
+    packages = ["ScreenAPI"],
+    version = "Beta 1.0",
+    description = "A python library used for making custom screens easier.",
+    author = "IKings",
+    author_email = "adking08@hotmail.com",
+    url = "https://github.com/KingsMMA/ScreenAPI",
+    download_url = "https://github.com/KingsMMA/ScreenAPI/archive/beta_v10.tar.gz",
+    keywords = ["screen", "api", "screenapi", "utility", "tkinter"],
+    install_requires = [
+        "tkinter",
+        "math"
+    ]
+    classifiers =[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3.0',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
